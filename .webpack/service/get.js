23e97472bfce4780dda099574d410c5a3846f225
Rendy Febry <rendyfebry@gmail.com>
2017-10-21 23:43:06 +0700

    Add get function

--- .webpack/service/get.js
+++ .webpack/service/get.js
@@ -85,44 +85,49 @@ var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 var main = exports.main = function () {
 	var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(event, context, callback) {
-		var data, params;
+		var params, result;
 		return _regenerator2.default.wrap(function _callee$(_context) {
 			while (1) {
 				switch (_context.prev = _context.next) {
 					case 0:
-						data = JSON.parse(event.body);
 						params = {
 							TableName: "notes",
-							Item: {
+							// 'Key' defines the partition key and sort key of the item to be retrieved
+							// - 'userId': Identity Pool identity id of the authenticated user
+							// - 'noteId': path parameter
+							Key: {
 								userId: event.requestContext.identity.cognitoIdentityId,
-								noteId: _uuid2.default.v1(),
-								content: data.content,
-								attachment: data.attachment,
-								createdAt: new Date().getTime()
+								noteId: event.pathParameters.id
 							}
 						};
-						_context.prev = 2;
-						_context.next = 5;
-						return dynamoDbLib.call('put', params);
+						_context.prev = 1;
+						_context.next = 4;
+						return dynamoDbLib.call("get", params);
 
-					case 5:
-						callback(null, (0, _responseLib.success)(params.Item));
-						_context.next = 12;
+					case 4:
+						result = _context.sent;
+
+						if (result.Item) {
+							// Return the retrieved item
+							callback(null, (0, _responseLib.success)(result.Item));
+						} else {
+							callback(null, (0, _responseLib.failure)({ status: false, error: "Item not found." }));
+						}
+						_context.next = 11;
 						break;
 
 					case 8:
 						_context.prev = 8;
-						_context.t0 = _context['catch'](2);
+						_context.t0 = _context["catch"](1);
 
-						console.log(_context.t0);
 						callback(null, (0, _responseLib.failure)({ status: false }));
 
-					case 12:
-					case 'end':
+					case 11:
+					case "end":
 						return _context.stop();
 				}
 			}
-		}, _callee, this, [[2, 8]]);
+		}, _callee, this, [[1, 8]]);
 	}));
 
 	return function main(_x, _x2, _x3) {
@@ -130,15 +135,11 @@ var main = exports.main = function () {
 	};
 }();
 
-var _uuid = __webpack_require__(3);
-
-var _uuid2 = _interopRequireDefault(_uuid);
-
-var _dynamodbLib = __webpack_require__(4);
+var _dynamodbLib = __webpack_require__(3);
 
 var dynamoDbLib = _interopRequireWildcard(_dynamodbLib);
 
-var _responseLib = __webpack_require__(6);
+var _responseLib = __webpack_require__(5);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
@@ -158,12 +159,6 @@ module.exports = require("babel-runtime/helpers/asyncToGenerator");
 
 /***/ }),
 /* 3 */
-/***/ (function(module, exports) {
-
-module.exports = require("uuid");
-
-/***/ }),
-/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -174,7 +169,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.call = call;
 
-var _awsSdk = __webpack_require__(5);
+var _awsSdk = __webpack_require__(4);
 
 var _awsSdk2 = _interopRequireDefault(_awsSdk);
 
@@ -189,13 +184,13 @@ function call(action, params) {
 }
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function(module, exports) {
 
 module.exports = require("aws-sdk");
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -205,7 +200,7 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-var _stringify = __webpack_require__(7);
+var _stringify = __webpack_require__(6);
 
 var _stringify2 = _interopRequireDefault(_stringify);
 
@@ -234,7 +229,7 @@ function buildResponse(statusCode, body) {
 }
 
 /***/ }),
-/* 7 */
+/* 6 */
 /***/ (function(module, exports) {
 
 module.exports = require("babel-runtime/core-js/json/stringify");
