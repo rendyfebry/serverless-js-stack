6267a0e1d93c2b4460c70742cc0eb109b58c25de
Rendy Febry <rendyfebry@gmail.com>
2017-10-21 23:49:02 +0700

    Add Delete Function

--- update.js
+++ update.js
@@ -1,8 +1,8 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDbLib from "./libs/dynamodb-lib"
+import { success, failure } from "./libs/response-lib"
 
 export async function main(event, context, callback) {
-	const data = JSON.parse(event.body);
+	const data = JSON.parse(event.body)
 	const params = {
 		TableName: "notes",
 		// 'Key' defines the partition key and sort key of the item to be updated
@@ -20,12 +20,12 @@ export async function main(event, context, callback) {
 			":content": data.content ? data.content : null
 		},
 		ReturnValues: "ALL_NEW"
-	};
+	}
 
 	try {
-		const result = await dynamoDbLib.call("update", params);
-		callback(null, success({ status: true }));
+		const result = await dynamoDbLib.call("update", params)
+		callback(null, success({ status: true }))
 	} catch (e) {
-		callback(null, failure({ status: false }));
+		callback(null, failure({ status: false }))
 	}
 }
